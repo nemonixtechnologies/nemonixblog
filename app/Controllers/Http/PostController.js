1d11a9bc204552f3506e899880f3dd143f40a8a2
derekaustinz <36425978+derekmanu@users.noreply.github.com>
2019-09-09 22:01:06 +1000

    first commit

---
+++ app/Controllers/Http/PostController.js
@@ -0,0 +1,42 @@
+'use strict'
+
+//Bring in model
+const Post = use('App/Models/Post')
+
+class PostController {
+    async index({ view }){
+
+        const posts = await Post.all();
+        return view.render('posts.index', {
+        title: 'Latest post',
+        posts: posts.toJSON()
+    })
+  }
+
+    async details({ params, view }) {
+        const post = await Post.find(params.id)
+        
+        return view.render('posts.details',{
+            post: post
+        })
+    }
+
+    async add({ view }) {
+        return view.render('posts.add')
+    }
+
+    async store({ request, response, session }) {
+        const post = new Post();
+
+        post.title = request.input('title')
+        post.body = request.input('body')
+
+        await post.save()
+
+        session.flash({ notification: 'Post Added!'})
+
+        return response.redirect('/posts')
+    }
+}
+
+module.exports = PostController
